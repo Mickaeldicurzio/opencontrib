306574e49e6015082463d7a03196afb9734ed11e
mdicurzio <mickael.dicurzio@gmail.com>
2017-10-06 11:34:49 +0200

    final

--- src/OpenContrib/UserBundle/Entity/User.php
+++ src/OpenContrib/UserBundle/Entity/User.php
@@ -24,7 +24,7 @@ class User extends BaseUser
     protected $id;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)=
+     * @ORM\Column(type="string", length=255, nullable=true)
      * @var string $name
      */
     private $name;
